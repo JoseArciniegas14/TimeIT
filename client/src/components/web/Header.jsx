1306f86925550290c4060bbb76d42fe0fc8442df
Jose Arciniegas <josesebastianarciniegasvargas@gmail.com>
2023-12-06 17:20:37 -0500

    prettier

--- client/src/components/web/Header.jsx
+++ client/src/components/web/Header.jsx
@@ -2,31 +2,32 @@ import { NavLink } from "react-router-dom";
 import { BtnUser } from "./BtnUser";
 import "../../css/inicio/style.css";
 
-
 function HeaderNav() {
   return (
     <nav className="MENU relative w-full h-24">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="items-center justify-between h-16">
           <div className="flex-shrink-0">
-          <div className="logo-with-text absolute w-48 h-12">
-            <img className="icono-menu absolute w-12 h-12 top-0 left-0" src="client\src\assets\img\ICONO.svg" />
-        <div className="titulo absolute h-10 font-semibold text-3xl text-center tracking-normal">TimeiT</div>
-        </div>
+            <div className="logo-with-text absolute w-48 h-12">
+              <img
+                className="icono-menu absolute w-12 h-12 top-0 left-0"
+                src="client\src\assets\img\ICONO.svg"
+              />
+              <div className="titulo absolute h-10 font-semibold text-3xl text-center tracking-normal">
+                TimeiT
+              </div>
+            </div>
           </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4 ">
-              
               <NavLink
-              
                 to="/home"
                 activeClassName="text-white"
                 className="text-gray-300 text-xl hover:text-white font-bold tracking-normal text-center no-underline px-3 py-2 rounded-md"
               >
-                
                 Inicio
               </NavLink>
-              
+
               <NavLink
                 to="/alarms"
                 activeClassName="text-white"
@@ -50,7 +51,6 @@ function HeaderNav() {
               </NavLink>
             </div>
           </div>
-          
         </div>
         <BtnUser />
       </div>
